abe77ed8553a73c7e3359b91969e98074c01ef34
Daniel Steman <daniel-steman@live.nl>
2024-05-11 13:13:35 +0200

    add request builder

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"time"
@@ -84,6 +85,32 @@ func GetToken(client *http.Client, credentials Credentials) (*Token, error) {
 	return &token, nil
 }
 
+func BuildAuthorizedRequest(method, url, token string, body io.Reader, contentType string) (*http.Request, error) {
+
+	if method != "GET" && method != "POST" {
+	    return nil, fmt.Errorf("unsupported HTTP method: %s", method)
+	}
+	if url == "" {
+	    return nil, errors.New("URL cannot be empty")
+	}
+
+
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+	    return nil, fmt.Errorf("error creating request: %v", err)
+	}
+
+
+	req.Header.Set("accept", "application/json")
+	if contentType != "" {
+	    req.Header.Set("Content-Type", contentType)
+	}
+	req.Header.Set("Authorization", token)
+
+	return req, nil
+}
+
+
 func GoCardLessManager() {
 	credentials, err := GetCredentials()
 	if err != nil {
@@ -99,6 +126,31 @@ func GoCardLessManager() {
 	}
 	fmt.Println("Token:", string(token.Access))
 }
+
+type Bank struct {
+	ID                   string   `json:"id"`
+	Name                 string   `json:"name"`
+	BIC                  string   `json:"bic"`
+	TransactionTotalDays int      `json:"transaction_total_days"`
+	Countries            []string `json:"countries"`
+	Logo                 string   `json:"logo"`
+}
+
+func GetBanksInCountry(client *http.Client, countryCode string) ([]Bank, error) {
+	URL := fmt.Sprintf("https://bankaccountdata.gocardless.com/api/v2/institutions/?country=%s", countryCode)
+	resp, err := client.Get(URL)
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %v", err)
+	}
+	defer resp.Body.Close()
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response body: %v", err)
+	}
+	fmt.Println("Token:", string(responseBody))
+	return []Bank{}, err
+}
+
 
 
 func main() {
