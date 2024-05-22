4a8bc2c821867948a5b2ec959055dd69c113fdef
Daniel Steman <daniel-steman@live.nl>
2024-05-22 19:26:13 +0200

    add viper

---
+++ config/viper.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+type AppConfig struct {
+	DbURL        string `mapstructure:"DB_URL"`
+	DbDriver     string `mapstructure:"DB_DRIVER"`
+	ServeAddress string `mapstructure:"SERVE_ADDRESS"`
+}
+
+func LoadAppConfig() (AppConfig, error) {
+	path := "settings.yml"
+	viper.AddConfigPath(path)
+	var config AppConfig
+	if err := viper.Unmarshal(&config); err != nil {
+		return AppConfig{}, fmt.Errorf("failed to parse config: %w", err)
+	}
+
+	return config, nil
+}
