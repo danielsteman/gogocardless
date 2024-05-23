2d47181baa17eba920d0da784489ad077f58ee45
Daniel Steman <daniel-steman@live.nl>
2024-05-23 09:03:15 +0200

    wip

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/danielsteman/gogocardless/config"
+)
+
+func main() {
+	config, err := config.LoadAppConfig()
+	if err != nil {
+		log.Fatal("Couldn't get banks:", err)
+	}
+	fmt.Println("test:", string(config.SecretID))
 }
