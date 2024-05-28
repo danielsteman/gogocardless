4911c0345aa9d896619cd2f256b6e85108e06ce2
Daniel Steman <daniel-steman@live.nl>
2024-05-28 22:08:58 +0200

    create bank list route

--- cmd/bank.go
+++ cmd/bank.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/danielsteman/gogocardless/gocardless"
@@ -27,23 +29,58 @@ type Bank struct {
 	Logo                 string   `json:"logo"`
 }
 
-type ListBanksResponse struct {
-	Banks   []*Bank
-	Elapsed int64 `json:"elapsed"`
+func (b *Bank) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (b *Bank) Bind(r *http.Request) error {
+	return nil
 }
 
 func ListBanks(w http.ResponseWriter, r *http.Request) {
 	token, err := gocardless.GetOrRefreshToken()
-
 	if err != nil {
 		http.Error(w, fmt.Sprintf("failed to get token: %v", err), http.StatusInternalServerError)
 		return
 	}
 
-	tokenResponse := gocardless.TokenResponse{
-		Token:   token,
-		Elapsed: 0,
+	url := "https://bankaccountdata.gocardless.com/api/v2/institutions/?country=nl"
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("failed to create request: %v", err), http.StatusInternalServerError)
+		return
 	}
-	render.Status(r, http.StatusCreated)
-	render.Render(w, r, &tokenResponse)
+
+	req.Header.Set("accept", "application/json")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+token.Access)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("failed to get banks: %v", err), http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+	jsonData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("failed to read response body: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	var banks []Bank
+	err = json.Unmarshal([]byte(jsonData), &banks)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("failed to parse json: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	bankList := make([]render.Renderer, len(banks))
+	for i, bank := range banks {
+		bankList[i] = &bank
+	}
+
+	render.Status(r, http.StatusOK)
+	render.RenderList(w, r, bankList)
 }
