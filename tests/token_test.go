a666c95801c621c7782e17cfdd7b54140173ebc8
Daniel Steman <daniel-steman@live.nl>
2024-05-28 18:52:28 +0200

    run migrations for test

--- tests/token_test.go
+++ tests/token_test.go
@@ -1,10 +1,29 @@
 package tests
 
 import (
+	"log"
+	"os"
 	"testing"
 
+	"github.com/danielsteman/gogocardless/db"
 	"github.com/danielsteman/gogocardless/gocardless"
 )
+
+func TestMain(m *testing.M) {
+	db, err := db.GetDB("gogocardless-test")
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %v", err)
+	}
+
+	err = db.AutoMigrate(&gocardless.Token{})
+	if err != nil {
+		panic("failed to migrate database")
+	}
+
+	exitCode := m.Run()
+
+	os.Exit(exitCode)
+}
 
 func TestCreateToken(t *testing.T) {
 	_, err := gocardless.GetOrRefreshToken()
