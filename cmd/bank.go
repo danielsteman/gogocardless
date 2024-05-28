8fd72f567a1d753092da46b924689e3ed96d2bed
Daniel Steman <daniel-steman@live.nl>
2024-05-28 10:14:41 +0200

    remove comments

--- cmd/bank.go
+++ cmd/bank.go
@@ -17,16 +17,7 @@ type bankResource struct{}
 
 func (rs bankResource) Routes() chi.Router {
 	r := chi.NewRouter()
-	r.Get("/list", ListBanks) // GET /users - read a list of users
-	// r.Post("/", rs.Create) // POST /users - create a new user and persist it
-	// r.Put("/", rs.Delete)
-
-	// r.Route("/{id}", func(r chi.Router) {
-	// 	// r.Use(rs.TodoCtx) // lets have a users map, and lets actually load/manipulate
-	// 	r.Get("/", rs.Get)       // GET /users/{id} - read a single user by :id
-	// 	r.Put("/", rs.Update)    // PUT /users/{id} - update a single user by :id
-	// 	r.Delete("/", rs.Delete) // DELETE /users/{id} - delete a single user by :id
-	// })
+	r.Get("/list", ListBanks)
 
 	return r
 }
