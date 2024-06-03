de3c2af5c9e6a2106d6ee9eb6773d5dc3561338e
Daniel Steman <daniel-steman@live.nl>
2024-06-03 16:41:55 +0200

    create route to get user redirect link

--- cmd/user.go
+++ cmd/user.go
@@ -1,12 +1,39 @@
 package main
 
-import "github.com/go-chi/chi/v5"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/danielsteman/gogocardless/gocardless"
+	"github.com/go-chi/chi/v5"
+)
 
 type userResource struct{}
 
 func (rs userResource) Routes() chi.Router {
 	r := chi.NewRouter()
-	// r.Get("/redirect", ListBanks)
+	r.Get("/redirect", userRedirectHandler)
 
 	return r
+}
+
+func userRedirectHandler(w http.ResponseWriter, r *http.Request) {
+	institutionID := r.URL.Query().Get("institutionId")
+	if institutionID == "" {
+		http.Error(w, "institutionId query parameter is required", http.StatusBadRequest)
+		return
+	}
+
+	redirectInfo, err := gocardless.GetEndUserRequisitionLink(institutionID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(redirectInfo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
