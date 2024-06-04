79daad4f6dd57f87d31b0c6f84a3f5a4d46cb42f
Daniel Steman <daniel-steman@live.nl>
2024-06-04 15:59:23 +0200

    style

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -19,8 +19,8 @@ async function fetchBanks(): Promise<Bank[]> {
 
 function BanksList({ banks }: { banks: Bank[] }) {
   return (
-    <div>
-      <h1>Banks List</h1>
+    <div className='px-16 py-8'>
+      <h1 className='text-2xl pb-8 font-bold'>Banks</h1>
       <div>
         {banks.map(bank => (
           <div key={bank.id}>{bank.name}</div>
