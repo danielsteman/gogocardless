08021e2dfa64dfd249a76f58d0f18df3d87a2f5a
Daniel Steman <daniel-steman@live.nl>
2024-06-05 08:01:56 +0200

    add middleware

--- web/src/app/api/auth/[...nextauth]/route.ts
+++ web/src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,3 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import { handlers } from "@/app/auth";
 
-export const options = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-    })
-  ],
-};
-
-export const handler = NextAuth(options);
-
-export { handler as GET, handler as POST };
+export const { GET, POST } = handlers;
