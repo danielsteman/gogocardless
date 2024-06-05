52b1ed93831e032c2fea251ad5b625af23242414
Daniel Steman <daniel-steman@live.nl>
2024-06-05 07:59:19 +0200

    move nextauth options

--- web/src/app/auth.ts
+++ web/src/app/auth.ts
@@ -1,15 +1,12 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const options = {
+export const {handlers, auth, signIn, signOut} = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
     })
   ],
-};
+});
 
-export const handler = NextAuth(options);
-
-export { handler as GET, handler as POST };
